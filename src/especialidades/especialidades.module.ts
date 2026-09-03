d8cbfe8267c5b338f0b0efc5b3e1d9fbc85cb316
Milton <miltonhibero@gmail.com>
2026-09-03 18:59:46 -0500

    Completar modulo de especialidades

---
+++ src/especialidades/especialidades.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { EspecialidadesController } from './especialidades.controller';
+import { EspecialidadesService } from './especialidades.service';
+import { Especialidad } from './entities/especialidad.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Especialidad])],
+  controllers: [EspecialidadesController],
+  providers: [EspecialidadesService],
+  exports: [EspecialidadesService],
+})
+export class EspecialidadesModule {}
