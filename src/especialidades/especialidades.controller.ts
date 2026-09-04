2c6ff6fa8addbcdd71c7ba9102a6e6422843abba
Milton <miltonhibero@gmail.com>
2026-09-04 11:04:26 -0500

    Implementar modulo de especialidades

---
+++ src/especialidades/especialidades.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+import { EspecialidadesService } from './especialidades.service';
+import { CreateEspecialidadDto } from './dto/create-especialidad.dto';
+import { UpdateEspecialidadDto } from './dto/update-especialidad.dto';
+
+@Controller('especialidades')
+export class EspecialidadesController {
+  constructor(
+    private readonly especialidadesService: EspecialidadesService,
+  ) {}
+
+  @Post()
+  create(@Body() createEspecialidadDto: CreateEspecialidadDto) {
+    return this.especialidadesService.create(createEspecialidadDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.especialidadesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.especialidadesService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateEspecialidadDto: UpdateEspecialidadDto,
+  ) {
+    return this.especialidadesService.update(id, updateEspecialidadDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.especialidadesService.remove(id);
+  }
+}
